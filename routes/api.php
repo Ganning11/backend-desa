de111dac0a1c1140f992f67c8a715741069004a5
Ganaldi Gilang <60030876+Ganning11@users.noreply.github.com>
2024-01-10 10:24:52 +0700

    create api Roles and User (plus setengah jadi permission)

--- routes/api.php
+++ routes/api.php
@@ -13,12 +13,32 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::post('/logout', [App\Http\Controllers\Api\Auth\LoginController::class, 'logout']);
 });
 
-
 //group route with prefix "admin"
 Route::prefix('admin')->group(function () {
     //group route with middleware "auth:api"
     Route::group(['middleware' => 'auth:api'], function () {
+
         //dashboard
         Route::get('/dashboard', App\Http\Controllers\Api\Admin\DashboardController::class);
+
+        //permissions
+        Route::get('/permissions', [\App\Http\Controllers\Api\Admin\PermissionController::class, 'index'])
+            ->middleware('permission:permissions.index');
+
+        //permissions all
+        Route::get('/permissions/all', [\App\Http\Controllers\Api\Admin\PermissionController::class, 'all'])
+            ->middleware('permission:permissions.index');
+
+        //roles all
+        Route::get('/roles/all', [\App\Http\Controllers\Api\Admin\RoleController::class, 'all'])
+            ->middleware('permission:roles.index');
+
+        //roles
+        Route::apiResource('/roles', App\Http\Controllers\Api\Admin\RoleController::class)
+            ->middleware('permission:roles.index|roles.store|roles.update|roles.delete');
+
+        //users
+        Route::apiResource('/users', App\Http\Controllers\Api\Admin\UserController::class)
+            ->middleware('permission:users.index|users.store|users.update|users.delete');
     });
 });
