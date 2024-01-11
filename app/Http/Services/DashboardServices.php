1364c4671529c09374b2bdbc057c366e6b2492ac
Donytriprasetyo <donyprasetyo84@gmail.com>
2024-01-11 16:50:43 +0700

    Adding Dashboard Services

---
+++ app/Http/Services/DashboardServices.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Post;
+use App\Models\Product;
+use App\Models\Aparatur;
+use App\Models\Category;
+use App\Http\Controllers\Controller;
+
+class DashboardServices
+{
+    /**
+     * Handle the incoming request.
+     */
+    public static function index()
+    {
+        //count categories
+        $categories = Category::count();
+
+        //count posts
+        $posts = Post::count();
+
+        //count products
+        $products = Product::count();
+
+        //count aparaturs
+        $aparaturs = Aparatur::count();
+
+        //return response json
+        return response()->json([
+            'success'   => true,
+            'message'   => 'List Data on Dashboard',
+            'data'      => [
+                'categories' => $categories,
+                'posts'      => $posts,
+                'products'   => $products,
+                'aparaturs'  => $aparaturs,
+            ]
+        ]);
+    }
+}
