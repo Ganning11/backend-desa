035267de3b0b00a91f65842d6b0583e3bfd1ebfd
Donytriprasetyo <donyprasetyo84@gmail.com>
2024-01-11 16:59:46 +0700

    Adding PageServices

--- app/Http/Controllers/Api/Admin/PageController.php
+++ app/Http/Controllers/Api/Admin/PageController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PageResource;
+use App\Services\PageServices;
 use Illuminate\Support\Facades\Validator;
 
 class PageController extends Controller
@@ -18,16 +19,11 @@ class PageController extends Controller
      */
     public function index()
     {
-        //get pages
-        $pages = Page::when(request()->search, function ($pages) {
-            $pages = $pages->where('title', 'like', '%' . request()->search . '%');
-        })->latest()->paginate(5);
-
-        //append query string to pagination links
-        $pages->appends(['search' => request()->search]);
-
-        //return with Api Resource
-        return new PageResource(true, 'List Data Pages', $pages);
+        try {
+            return PageServices::index();
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     /**
@@ -38,30 +34,11 @@ class PageController extends Controller
      */
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'title'     => 'required',
-            'content'   => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
+        try {
+            return PageServices::store($request);
+        } catch (\Throwable $th) {
+            throw $th;
         }
-
-        //create page
-        $page = Page::create([
-            'title'     => $request->title,
-            'slug'      => Str::slug($request->title),
-            'content'   => $request->content,
-            'user_id'   => auth()->guard('api')->user()->id
-        ]);
-
-        if ($page) {
-            //return success with Api Resource
-            return new PageResource(true, 'Data Page Berhasil Disimpan!', $page);
-        }
-
-        //return failed with Api Resource
-        return new PageResource(false, 'Data Page Gagal Disimpan!', null);
     }
 
     /**
@@ -72,15 +49,11 @@ class PageController extends Controller
      */
     public function show($id)
     {
-        $page = Page::whereId($id)->first();
-
-        if ($page) {
-            //return success with Api Resource
-            return new PageResource(true, 'Detail Data Page!', $page);
+        try {
+            return PageServices::show($id);
+        } catch (\Throwable $th) {
+            throw $th;
         }
-
-        //return failed with Api Resource
-        return new PageResource(false, 'Detail Data Page Tidak DItemukan!', null);
     }
 
     /**
@@ -92,30 +65,11 @@ class PageController extends Controller
      */
     public function update(Request $request, Page $page)
     {
-        $validator = Validator::make($request->all(), [
-            'title'     => 'required',
-            'content'   => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
+        try {
+            return PageServices::update($request, $page);
+        } catch (\Throwable $th) {
+            throw $th;
         }
-
-        //update page
-        $page->update([
-            'title'     => $request->title,
-            'slug'      => Str::slug($request->title),
-            'content'   => $request->content,
-            'user_id'   => auth()->guard('api')->user()->id
-        ]);
-
-        if ($page) {
-            //return success with Api Resource
-            return new PageResource(true, 'Data Page Berhasil Diupdate!', $page);
-        }
-
-        //return failed with Api Resource
-        return new PageResource(false, 'Data Page Gagal Diupdate!', null);
     }
 
     /**
@@ -126,12 +80,10 @@ class PageController extends Controller
      */
     public function destroy(Page $page)
     {
-        if ($page->delete()) {
-            //return success with Api Resource
-            return new PageResource(true, 'Data Page Berhasil Dihapus!', null);
+        try {
+            return PageServices::destroy($page);
+        } catch (\Throwable $th) {
+            throw $th;
         }
-
-        //return failed with Api Resource
-        return new PageResource(false, 'Data Page Gagal Dihapus!', null);
     }
 }
