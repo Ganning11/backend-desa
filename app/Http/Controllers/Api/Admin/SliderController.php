176f26823df0f5f21e5c380b0a9051ffd3ceb13e
Donytriprasetyo <donyprasetyo84@gmail.com>
2024-01-12 08:48:11 +0700

    Adding SliderServices

--- app/Http/Controllers/Api/Admin/SliderController.php
+++ app/Http/Controllers/Api/Admin/SliderController.php
@@ -6,6 +6,7 @@ use App\Models\Slider;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\SliderResource;
+use App\Services\SliderServices;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
@@ -18,11 +19,11 @@ class SliderController extends Controller
      */
     public function index()
     {
-        //get data sliders
-        $sliders = Slider::latest()->paginate(5);
-
-        //return with Api Resource
-        return new SliderResource(true, 'List Data Sliders', $sliders);
+        try {
+            return SliderServices::index();
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     /**
@@ -33,29 +34,11 @@ class SliderController extends Controller
      */
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'image' => 'required|image|mimes:jpeg,jpg,png|max:2000',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
+        try {
+            return SliderServices::store($request);
+        } catch (\Throwable $th) {
+            throw $th;
         }
-
-        //upload image
-        $image = $request->file('image');
-        $image->storeAs('public/sliders', $image->hashName());
-
-        $slider = Slider::create([
-            'image' => $image->hashName(),
-        ]);
-
-        if ($slider) {
-            //return success with Api Resource
-            return new SliderResource(true, 'Data Slider Berhasil Disimpan!', $slider);
-        }
-
-        //return failed with Api Resource
-        return new SliderResource(false, 'Data Slider Gagal Disimpan!', null);
     }
 
     /**
@@ -66,15 +49,10 @@ class SliderController extends Controller
      */
     public function destroy(Slider $slider)
     {
-        //remove image
-        Storage::disk('local')->delete('public/sliders/' . basename($slider->image));
-
-        if ($slider->delete()) {
-            //return success with Api Resource
-            return new SliderResource(true, 'Data Slider Berhasil Dihapus!', null);
+        try {
+            return SliderServices::destroy($slider);
+        } catch (\Throwable $th) {
+            throw $th;
         }
-
-        //return failed with Api Resource
-        return new SliderResource(false, 'Data Slider Gagal Dihapus!', null);
     }
 }
