f74a0ff328edacd76cd23c7a96d988deca75a153
Donytriprasetyo <donyprasetyo84@gmail.com>
2024-01-12 08:39:30 +0700

    Adding ProductServices

--- app/Http/Controllers/Api/Admin/ProductController.php
+++ app/Http/Controllers/Api/Admin/ProductController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Resources\ProductResource;
+use App\Services\ProductServices;
 use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
@@ -19,16 +20,11 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //get products
-        $products = Product::when(request()->search, function ($products) {
-            $products = $products->where('title', 'like', '%' . request()->search . '%');
-        })->latest()->paginate(5);
-
-        //append query string to pagination links
-        $products->appends(['search' => request()->search]);
-
-        //return with Api Resource
-        return new ProductResource(true, 'List Data Products', $products);
+        try {
+            return ProductServices::index();
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     /**
@@ -39,44 +35,11 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'image'    => 'required|mimes:jpeg,jpg,png|max:2000',
-            'title'    => 'required',
-            'content'  => 'required',
-            'owner'    => 'required',
-            'price'    => 'required',
-            'address'  => 'required',
-            'phone'    => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
+        try {
+            return ProductServices::store($request);
+        } catch (\Throwable $th) {
+            throw $th;
         }
-
-        //upload image
-        $image = $request->file('image');
-        $image->storeAs('public/products', $image->hashName());
-
-        //create Product
-        $product = Product::create([
-            'image' => $image->hashName(),
-            'title' => $request->title,
-            'slug'  => Str::slug($request->title, '-'),
-            'content' => $request->content,
-            'owner' => $request->owner,
-            'price' => $request->price,
-            'address' => $request->address,
-            'phone' => $request->phone,
-            'user_id'     => auth()->guard('api')->user()->id,
-        ]);
-
-        if ($product) {
-            //return success with Api Resource
-            return new ProductResource(true, 'Data Product Berhasil Disimpan!', $product);
-        }
-
-        //return failed with Api Resource
-        return new ProductResource(false, 'Data Product Gagal Disimpan!', null);
     }
 
     /**
@@ -87,15 +50,11 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        $product = Product::whereId($id)->first();
-
-        if ($product) {
-            //return success with Api Resource
-            return new ProductResource(true, 'Detail Data Product!', $product);
+        try {
+            return ProductServices::show($id);
+        } catch (\Throwable $th) {
+            throw $th;
         }
-
-        //return failed with Api Resource
-        return new ProductResource(false, 'Detail Data Product Tidak DItemukan!', null);
     }
 
     /**
@@ -107,62 +66,11 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        $validator = Validator::make($request->all(), [
-            'title'    => 'required',
-            'content'  => 'required',
-            'owner'    => 'required',
-            'price'    => 'required',
-            'address'  => 'required',
-            'phone'    => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
+        try {
+            return ProductServices::update($request, $product);
+        } catch (\Throwable $th) {
+            throw $th;
         }
-
-        //check image update
-        if ($request->file('image')) {
-
-            //remove old image
-            Storage::disk('local')->delete('public/products/' . basename($product->image));
-
-            //upload new image
-            $image = $request->file('image');
-            $image->storeAs('public/products', $image->hashName());
-
-            //update Product with new image
-            $product->update([
-                'image' => $image->hashName(),
-                'title' => $request->title,
-                'slug'  => Str::slug($request->title, '-'),
-                'content' => $request->content,
-                'owner' => $request->owner,
-                'price' => $request->price,
-                'address' => $request->address,
-                'phone' => $request->phone,
-                'user_id'     => auth()->guard('api')->user()->id,
-            ]);
-        }
-
-        //update Product without image
-        $product->update([
-            'title' => $request->title,
-            'slug'  => Str::slug($request->title, '-'),
-            'content' => $request->content,
-            'owner' => $request->owner,
-            'price' => $request->price,
-            'address' => $request->address,
-            'phone' => $request->phone,
-            'user_id'     => auth()->guard('api')->user()->id,
-        ]);
-
-        if ($product) {
-            //return success with Api Resource
-            return new ProductResource(true, 'Data Product Berhasil Diupdate!', $product);
-        }
-
-        //return failed with Api Resource
-        return new ProductResource(false, 'Data Product Gagal Diupdate!', null);
     }
 
     /**
@@ -173,15 +81,10 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //remove image
-        Storage::disk('local')->delete('public/products/' . basename($product->image));
-
-        if ($product->delete()) {
-            //return success with Api Resource
-            return new ProductResource(true, 'Data Product Berhasil Dihapus!', null);
+        try {
+            return ProductServices::destroy($product);
+        } catch (\Throwable $th) {
+            throw $th;
         }
-
-        //return failed with Api Resource
-        return new ProductResource(false, 'Data Product Gagal Dihapus!', null);
     }
 }
